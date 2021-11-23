ac2694684fa126302b8589657cfc7db112657619
Patrick Möller <patrick_moeller@live.de>
2021-11-23 20:25:15 +0100

    Adding base styling to the card component

--- src/app/components/Card/Card.tsx
+++ src/app/components/Card/Card.tsx
@@ -4,9 +4,12 @@ import classes from './Card.module.css';
 export default function Card(): JSX.Element {
   return (
     <span className={classes.card}>
-      <title></title>
-      <p></p>
-      <input type="number" />
+      <title>Name des Gegenstandes</title>
+      <p>Beschreibung des Gegenstandes</p>
+      <label htmlFor="">
+        Gewicht:
+        <input type="number" />
+      </label>
     </span>
   );
 }
