c0d7d247bfa35ec203d4bb94b245ca8bc423e163
Patrick Möller <patrick_moeller@live.de>
2021-11-24 01:25:43 +0100

    Adding CP based styling to the card

--- src/app/components/Card/Card.tsx
+++ src/app/components/Card/Card.tsx
@@ -3,13 +3,23 @@ import classes from './Card.module.css';
 
 export default function Card(): JSX.Element {
   return (
-    <span className={classes.card}>
-      <title>Name des Gegenstandes</title>
-      <p>Beschreibung des Gegenstandes</p>
-      <label htmlFor="">
-        Gewicht:
-        <input type="number" />
-      </label>
-    </span>
+    <div className="card">
+      <div className="card--header">
+        <h2 className="glow">Name des Gegenstandes</h2>
+      </div>
+      <hr />
+      <div className="card--body">
+        <div className="card--content">
+          <h4>Art des Gegenstandes</h4>
+        </div>
+        <div className="card--content">
+          <h4>
+            Gewicht:
+            <input type="number" />
+          </h4>
+        </div>
+      </div>
+      <hr />
+    </div>
   );
 }
