f4382b375523b1ed5ac0199a1c0b02907c4c1582
Patrick Möller <patrick_moeller@live.de>
2021-11-29 23:03:44 +0100

    Changing as recommended in Code Review

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,21 +1,11 @@
 import React, { useState } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Card from './components/Card/Card';
 
 function App(): JSX.Element {
-  const [count, setCount] = useState<number>(0);
-
   return (
     <>
       <Card />
     </>
-    // <BrowserRouter>
-    //   <Routes>
-    //     <Route path="/">
-    //       <Card />
-    //     </Route>
-    //   </Routes>
-    // </BrowserRouter>
   );
 }
 
