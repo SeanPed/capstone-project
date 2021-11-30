760ce2128436997c562d666a0ac57fa260f213a4
Patrick Möller <patrick_moeller@live.de>
2021-11-30 02:31:11 +0100

    Removing unnecessary classNames

--- src/app/components/DeleteButton/DeleteButton.tsx
+++ src/app/components/DeleteButton/DeleteButton.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import './DeleteButton.module.css';
 
 export default function deleteButton(): JSX.Element {
-  return <button className="button">X</button>;
+  return <button>X</button>;
 }
