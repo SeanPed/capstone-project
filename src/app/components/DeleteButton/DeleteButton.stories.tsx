11ceb7161cac2b934c192763744a6d9270fd3c85
Patrick Möller <patrick_moeller@live.de>
2021-11-30 02:14:22 +0100

    Adding DeleteButton HTML Structures

---
+++ src/app/components/DeleteButton/DeleteButton.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import deleteButton from './DeleteButton';
+
+export default {
+  title: 'Component/DeleteButton',
+  component: deleteButton,
+};
+
+export const Cardtest = (): JSX.Element => <button />;
---
+++ src/app/components/DeleteButton/DeleteButton.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './DeleteButton.module.css';
+
+export default function deleteButton(): JSX.Element {
+  return (
+    <button type="button">
+      <p className="styled-X">X</p>
+    </button>
+  );
+}
