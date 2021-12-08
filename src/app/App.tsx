6a036f5648a9236382cdb0b05ce4ac3e2fd1a73e
Patrick Möller <patrick_moeller@live.de>
2021-12-08 18:44:40 +0100

    Fixing Displaying issue in App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MainPage from './pages/MainPage/MainPage';
 
 function App(): JSX.Element {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<h1>Hello World</h1>}></Route>
+        <Route path="/" element={<MainPage />}></Route>
         <Route path="/input" element={<h1>Hello World 2</h1>}></Route>
       </Routes>
     </BrowserRouter>
