db4d5dc5ae86bebf745a2c535636e32eb2c25f42
Patrick Möller <patrick_moeller@live.de>
2021-12-08 17:24:41 +0100

    Adding test styling to MainPage.tsx

---
+++ src/app/pages/MainPage/MainPage.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import MainPage from './MainPage';
+
+export default {
+  title: 'Page/MainPage',
+  component: MainPage,
+};
+
+export const mainPage = (): JSX.Element => <MainPage />;
---
+++ src/app/pages/MainPage/MainPage.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import classes from './MainPage.module.css';
+
+export default function MainPage(): JSX.Element {
+  return <h1></h1>;
+}
