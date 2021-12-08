537dca47742341ead8fe7c9903e94bf4f051af96
Patrick Möller <patrick_moeller@live.de>
2021-12-08 13:11:39 +0100

    Adding Routing Options

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App(): JSX.Element {
-  return <h1>Hello World!</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Hello World</h1>}></Route>
+        <Route path="/input" element={<h1>Hello World 2</h1>}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
