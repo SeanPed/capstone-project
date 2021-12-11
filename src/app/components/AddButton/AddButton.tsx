f4615f5ce8ce9b480bcd905f6375b329da430d5b
Patrick Möller <patrick_moeller@live.de>
2021-12-12 00:08:27 +0100

    Finalising feature Click

--- src/app/components/AddButton/AddButton.tsx
+++ src/app/components/AddButton/AddButton.tsx
@@ -9,10 +9,11 @@ export default function AddButton(): JSX.Element {
       data-augmented-ui="all-hexangle-up border"
       className={classes.button_area_div}
     >
-      <Link to="/input" />
-      <button className={classes.add_button_symbol}>
-        <GiLightBackpack size={42} />
-      </button>
+      <Link to="/input">
+        <button className={classes.add_button_symbol}>
+          <GiLightBackpack size={42} />
+        </button>
+      </Link>
     </div>
   );
 }
