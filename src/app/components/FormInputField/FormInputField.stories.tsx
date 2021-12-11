b14e04e2e10eb11a7d22ae91b4dd79de5a7b502a
Patrick Möller <patrick_moeller@live.de>
2021-12-12 00:55:11 +0100

    Adding basic structure

---
+++ src/app/components/FormInputField/FormInputField.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import FormInputField from './FormInputField';
+
+export default {
+  title: 'Component/FormInputField',
+  component: FormInputField,
+};
+
+export const Regular = (): JSX.Element => <FormInputField />;
