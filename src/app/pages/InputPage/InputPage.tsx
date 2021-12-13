187f156df8218d413770d70fc29939408f2dfad4
Patrick Möller <patrick_moeller@live.de>
2021-12-13 23:16:26 +0100

    Adding Basic Structure

--- src/app/pages/InputPage/InputPage.tsx
+++ src/app/pages/InputPage/InputPage.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Form from '../../components/Form/Form';
+import BackButton from '../../components/BackButton/BackButton';
 import classes from './InputPage.module.css';
 
 export default function InputPage(): JSX.Element {
   return (
-    <div className={classes.body_div}>
+    <div className={classes.input_page_div}>
+      <div className={classes.button_div}>
+        <BackButton />
+      </div>
       <div className={classes.placeholder_div}></div>
       <Form />
       <div className={classes.placeholder_div}></div>
