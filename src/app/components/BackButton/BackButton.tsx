19e5470a3974ff7a5308c93b7c1cb0c7fa37c1ed
Patrick Möller <patrick_moeller@live.de>
2021-12-13 22:57:04 +0100

    Adding some basic structure to the button

--- src/app/components/BackButton/BackButton.tsx
+++ src/app/components/BackButton/BackButton.tsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import classes from './BackButton.module.css';
 
 export default function BackButton(): JSX.Element {
-  return <h1>Hello WOrld!</h1>;
+  return (
+    <div
+      data-augmented-ui="all-hexangle-down border"
+      className={classes.button_area_div}
+    >
+      <Link to="/">
+        <button className={classes.back_button_symbol}></button>
+      </Link>
+    </div>
+  );
 }
